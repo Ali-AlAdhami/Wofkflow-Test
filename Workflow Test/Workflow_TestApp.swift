bfc2cda954345e7d40e3d89843bb5134eef44c53
Ali Al-Adhami <aliraad@utexas.edu>
2023-02-24 22:38:47 -0600

    first commit

---
+++ Workflow Test/Workflow_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workflow_TestApp.swift
+//  Workflow Test
+//
+//  Created by Ali Al-Adhami on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Workflow_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
